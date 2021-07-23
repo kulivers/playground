0b38402a1689031a7da65cb9e185b1176a8b57a3
kulivers <kulivers@mail.ru>
2021-07-23 15:46:34 +0300

    some changes

--- src/css/width-auto/width-auto.js
+++ src/css/width-auto/width-auto.js
@@ -1,21 +1,40 @@
 import React, {Component} from 'react'
 import './width-auto.css'
-import {Link, BrowserRouter, Route, useHistory, withRouter} from 'react-router-dom'
+import {Link, BrowserRouter, Route, withRouter, useHistory } from 'react-router-dom'
 
-class WidthAuto extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Link to="/width-100"> width 100%</Link>
-                    <Link to="/width-auto"> width auto</Link>
-                    <Route path="/width-100" render={<div className="width-100">div with width:100%</div>
-                    }/>
-                    {/*<div className="width-auto">div with width:auto</div>*/}
-                </div>
-            </BrowserRouter>
-        )
-    }
+const WidthAuto = (props) => {
+    let history = useHistory();
+    console.log(history)
+    return (
+        <BrowserRouter>
+            <div>
+                <Route path="/width-100-auto/:any" render={({match, ...props}) => {
+                    return (
+                        <>
+                            <Link to="/">
+                                <button>go back</button>
+                            </Link>
+                            <br/>
+                            <Link to={`${match.url}/width-100`}> width 100%</Link>
+                            <Link to={`${match.url}/width-auto`}> width-auto</Link>
+                        </>
+                    )
+                }}/>
+                <Route path='/width-100' render={p => {
+                    return (<div style={{border: '2px red solid'}}>
+                            <div className="width-100">div with width:100%; U CAN SCROOL ---></div>
+                        </div>
+                    )
+                }}/>
+                <Route path="/width-auto" render={p =>
+                    <div style={{border: '2px red solid'}}>
+                        <div className="width-auto">div with width:auto; NO SCROLL</div>
+                    </div>
+                }/>
+            </div>
+        </BrowserRouter>
+    )
+
 }
 
 export default WidthAuto;
