8b20cbfa9a522b215616117c43c2fb0153b7aafa
kulivers <kulivers@mail.ru>
2021-07-28 21:48:38 +0300

    everything works fine

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import WidthAuto from './css/width-auto/width-auto'
-import {BrowserRouter, Link, Route} from 'react-router-dom'
-import React from 'react';
-import Menu from './Menu'
+import WidthAuto from "./css/width-auto/width-auto";
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import React from "react";
+import Menu from "./Menu";
 
-
-import Button from '@material-ui/core/Button'
-
+import Button from "@material-ui/core/Button";
+import { cssPlaygroundsArr } from "./Menu";
 
 const App = () => {
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Menu} />
+      <Route
+        path="/*"
+        render={({ match }) =>
+          match.url === "/" ? null : (
+            <div style={{ display: "flex" }}>
+              <Link to="/" style={{ margin: "auto", marginTop: "10px" }}>
+                <Button variant="contained" color="primary">
+                  go back
+                </Button>
+              </Link>
+            </div>
+          )
+        }
+      />
+      <hr />
 
-    return (
-        <BrowserRouter>
-            <Route exact path="/" component={Menu}/>
-            <Route path="/*" render={({match}) => (
-                (match.url === '/') ? null : (
-                    <div style={{display: 'flex'}}>
-                        <Link to="/" style={{margin: 'auto', marginTop: '10px'}}><Button variant="contained"
-                                                                                         color="primary">go
-                            back</Button></Link>
-                    </div>
-                )
-            )}/>
-            <Route path="/width-100-auto-difference" component={WidthAuto}/>
-        </BrowserRouter>
-    )
-}
+      <Route path="/width-100-auto-difference" component={WidthAuto} />
+    </BrowserRouter>
+  );
+};
 
 export default App;
-
-
--- src/css/width-auto/width-auto.js
+++ src/css/width-auto/width-auto.js
@@ -1,38 +1,39 @@
-import React, {Component, useEffect} from 'react'
-import './width-auto.css'
-import {Link, BrowserRouter, Route, withRouter} from 'react-router-dom'
-import Paper from '@material-ui/core/Paper';
+import React, { Component, useEffect } from "react";
+import "./width-auto.css";
+import { Link, BrowserRouter, Route, withRouter } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
 
+const WidthAuto = ({ match }) => {
+  return (
+    <div>
+      <>
+        <Link to={`${match.path}/width-100`}> width 100%</Link>
+        <Link to={`${match.path}/width-auto`}> width-auto</Link>
+      </>
 
-const WidthAuto = (props) => {
-    const {history, match, location} = props
+      <Route
+        path="*/width-100"
+        render={(p) => {
+          return (
+            <div style={{ border: "2px red solid" }}>
+              <div className="width-100">
+                div with width:100%; U CAN SCROOL --->
+              </div>
+            </div>
+          );
+        }}
+      />
 
-    return (
-        <div>
-            <>
-
-                <Link to={`${match.path}/width-100`}> width 100%</Link>
-                <Link to={`${match.path}/width-auto`}> width-auto</Link>
-            </>
-
-
-            <Route path="*/width-100" render={p => {
-                return (<div style={{border: '2px red solid'}}>
-                        <div className="width-100">div with width:100%; U CAN SCROOL ---></div>
-                    </div>
-                )
-            }}/>
-
-
-            <Route path="*/width-auto" render={p =>
-                <div style={{border: '2px red solid'}}>
-                    <div className="width-auto">div with width:auto; NO SCROLL</div>
-                </div>
-            }/>
-        </div>
-    )
-
-}
+      <Route
+        path="*/width-auto"
+        render={(p) => (
+          <div style={{ border: "2px red solid" }}>
+            <div className="width-auto">div with width:auto; NO SCROLL</div>
+          </div>
+        )}
+      />
+    </div>
+  );
+};
 
 export default withRouter(WidthAuto);
-
